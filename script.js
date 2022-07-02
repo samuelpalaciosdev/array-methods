63abe8a477e7b9d819a539560d1608036b67be23
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-01 21:04:03 -0400

    Array forEach Method

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ const companies = [
 ];
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+// forEach
+
+// for (let i = 0; i < companies.length; i++) {
+//   console.log(companies[i]);
+// }
+
+companies.forEach((company) => {
+  console.log(company);
+});
